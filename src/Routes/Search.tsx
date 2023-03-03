28a76c271d8c9a7547d5b32b550e367ecc2be2f0
조정훈 <jeongh1021@naver.com>
2023-03-03 23:31:59 +0900

    init

---
+++ src/Routes/Search.tsx
@@ -0,0 +1,13 @@
+import { useLocation } from "react-router-dom";
+
+const Search = () => {
+  const location = useLocation();
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
+  return (
+    <>
+      <h1>{keyword}</h1>
+    </>
+  );
+};
+export default Search;
